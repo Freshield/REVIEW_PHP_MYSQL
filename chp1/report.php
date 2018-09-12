64fb1dfa0b7da2df34db3db69b27381bd0a7a980
freshield <zxdsw199182@gmail.com>
2018-09-12 16:03:45 +0800

    update

--- chp1/report.php
+++ chp1/report.php
@@ -19,12 +19,19 @@
     $alien_description = $_POST['description'];
     $fang_spotted = $_POST['fangspotted'];
     $email = $_POST['email'];
+    $how_many = $_POST['howmay'];
+    $name = $_POST['firstname'] . '' . $_POST['lastname'];
+    $what_they_did = $_POST['whattheydid'];
+    $other = $_POST['other'];
 
     echo 'Thanks for submitting the form.<br />';
     echo 'You were abducted ' . $when_it_happened;
     echo ' and were gone for ' . $how_long . '<br />';
+    echo 'Number of aliens: ' . $how_many . '<br />';
     echo 'Describe them: ' . $alien_description . '<br />';
+    echo 'The aliens did this: ' . $what_they_did . '<br />';
     echo 'Was Fang there? ' . $fang_spotted . '<br />';
+    echo 'Other comments: ' . $other . '<br />';
     echo 'Your email address is ' . $email;
     ?>
 </body>
