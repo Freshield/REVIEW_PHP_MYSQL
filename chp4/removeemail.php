3410b2541c9d87dfc473909e87090fa071841524
freshield <zxdsw199182@gmail.com>
2018-09-13 19:12:01 +0800

    done chp4

--- chp4/removeemail.php
+++ chp4/removeemail.php
@@ -1,23 +1,40 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: freshield
- * Date: 18-9-13
- * Time: 下午3:19
- */
+<img src="blankface.jpg" width="161" height="350" alt="" style="float:right" />
+<img name="elvislogo" src="elvislogo.gif" width="229" height="32" border="0" alt="Make Me Elvis" />
+<p>Please select the email addresses to delete from the email list and click Remove</p>
+<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <?php
+    /**
+     * Created by PhpStorm.
+     * User: freshield
+     * Date: 18-9-13
+     * Time: 下午3:19
+     */
 
-$dbc = mysqli_connect(
-    'localhost', 'root', '8888',
-    'elvis_store') or die('Error connecting to MySQL server.');
+    $dbc = mysqli_connect(
+        'localhost', 'root', '8888',
+        'elvis_store') or die('Error connecting to MySQL server.');
 
-$email = $_POST['email'];
+    if (isset($_POST['submit'])){
+        foreach ($_POST['todelete'] as $delete_id){
+            $query = "delete from email_list where id = $delete_id";
+            mysqli_query($dbc, $query);
+        }
+        echo "Customer removed <br/>";
+    }
 
-$query = "DELETE FROM email_list WHERE email = '$email'";
+    $query = "select * from email_list";
+    $result = mysqli_query($dbc, $query);
 
-$result = mysqli_query($dbc, $query) or die('Error querying.');
+    while ($row = mysqli_fetch_array($result)){
+        echo '<input type="checkbox" value="' .
+            $row['id'] . '" name="todelete[]" />';
+        echo $row['first_name'];
+        echo ' ' . $row['last_name'];
+        echo ' ' . $row['email'];
+        echo '<br/>';
+    }
+    mysqli_close($dbc);
 
-echo 'Customer removed: ' . $email;
-
-mysqli_close($dbc);
-
-?>
+    ?>
+    <input type="submit" name="submit" value="Remove"/>
+</form>
--- chp4/sendemail.php
+++ chp4/sendemail.php
@@ -1,3 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Make Me Elvis - Send Email</title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+<img src="blankface.jpg" width="161" height="350" alt="" style="float:right" />
+<img name="elvislogo" src="elvislogo.gif" width="229" height="32" border="0" alt="Make Me Elvis" />
+<p><strong>Private:</strong> For Elmer's use ONLY<br />
+    Write and send an email to mailing list members.</p>
 <?php
 /**
  * Created by PhpStorm.
@@ -9,20 +22,55 @@
 $from = 'elmer@makemeelevis.com';
 $subject = $_POST['subject'];
 $text = $_POST['elvismail'];
+$output_form = false;
 
+if (isset($_POST['submit'])) {
+    if ((!empty($subject) && (!empty($text)))) {
+        $dbc = mysqli_connect(
+            'localhost', 'root', '8888',
+            'elvis_store') or die('Error connecting to MySQL server.');
 
-$dbc = mysqli_connect(
-    'localhost', 'root', '8888',
-    'elvis_store') or die('Error connecting to MySQL server.');
+        $query = "SELECT * FROM email_list";
+        $result = mysqli_query($dbc, $query) or die('Error querying.');
 
-$query = "SELECT * FROM email_list";
-$result = mysqli_query($dbc, $query) or die('Error querying.');
+        while ($row = mysqli_fetch_array($result)) {
+            $msg = "Dear ....";
+            $to = $row['email'];
+            # mail()
+            echo 'Email sent to: ' . $to . '<br/>';
+        }
 
-while ($row = mysqli_fetch_array($result)) {
-    $msg = "Dear ....";
-    $to = $row['email'];
-    # mail()
-    echo 'Email sent to: ' . $to . '<br/>';
+        mysqli_close($dbc);
+    }
+    else {
+        echo 'You forgot the meail subject and/or body text.<br/>';
+        $output_form = true;
+
+    }
+}
+else {
+    $output_form = true;
 }
 
-mysqli_close($dbc);
+if ($output_form == true){
+    ?>
+    <br/>
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+        <label for="subject">Subject of email:</label><br />
+        <input id="subject" name="subject" type="text" size="30"
+        value="<?php echo $subject; ?>"/><br />
+        <label for="elvismail">Body of email:</label><br />
+        <textarea id="elvismail" name="elvismail" rows="8" cols="40">
+        <?php echo $text;?></textarea><br />
+        <input type="submit" name="submit" value="submit" />
+    </form>
+    <?php
+}
+?>
+</body>
+</html>
+
+
+
+
+
