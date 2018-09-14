3ea3a84b1a504574d21ab1572eec8412b56cc946
freshield <zxdsw199182@gmail.com>
2018-09-14 14:18:33 +0800

    done the timer testing

---
+++ chp5/addscore.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>Guitar Wars - Add Your High Score</title>
+  <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+  <h2>Guitar Wars - Add Your High Score</h2>
+
+<?php
+  define('GW_UPLOADPATH', '');
+
+  if (isset($_POST['submit'])) {
+    // Grab the score data from the POST
+    $name = $_POST['name'];
+    $score = $_POST['score'];
+    $screenshot = $_FILES['screenshot']['name'];
+
+    if (!empty($name) && !empty($score)) {
+        $target = GW_UPLOADPATH . $screenshot;
+        echo "$target<br/>";
+        echo $_FILES['screenshot']['tmp_name'] . "<br/>";
+        $ori_path = $_FILES['screenshot']['tmp_name'] . '/' . $screenshot;
+        echo "$ori_path<br/>";
+        echo $_FILES['screenshot']['error'] . '<br/>';
+        /*
+        if (move_uploaded_file($ori_path, $target)){
+            // Connect to the database
+            $dbc = mysqli_connect(
+                'localhost', 'root',
+                '8888', 'gwdb');
+
+            // Write the data to the database
+            $query = "INSERT INTO guitarwars VALUES (0, NOW(), '$name', '$score', '$screenshot')";
+            mysqli_query($dbc, $query);
+
+            // Confirm success with the user
+            echo '<p>Thanks for adding your new high score!</p>';
+            echo '<p><strong>Name:</strong> ' . $name . '<br />';
+            echo '<strong>Score:</strong> ' . $score . '</p>';
+            echo "$screenshot";
+            echo '<img src="' . GW_UPLOADPATH . $screenshot . ': alt="Score image" /></p>';
+            echo '<p><a href="index.php">&lt;&lt; Back to high scores</a></p>';
+
+            // Clear the score data to clear the form
+            $name = "";
+            $score = "";
+
+            mysqli_close($dbc);
+        } else {
+            echo "meet problem";
+        }*/
+
+
+    }
+    else {
+      echo '<p class="error">Please enter all of the information to add your high score.</p>';
+    }
+  }
+?>
+
+  <hr />
+  <form enctype="multipart/form-data" method="post"
+        action="<?php echo $_SERVER['PHP_SELF']; ?>">
+      <input type="hidden" name="MAX_FILE_SIZE" value="32768000"/>
+      <label for="name">Name:</label>
+      <input type="text" id="name" name="name" value="<?php if (!empty($name)) echo $name; ?>" /><br />
+      <label for="score">Score:</label>
+      <input type="text" id="score" name="score" value="<?php if (!empty($score)) echo $score; ?>" /><br/>
+      <br/>
+      <label for="screenshot">Screen shot:</label>
+      <input type="file" id="screenshot" name="screenshot"/>
+      <hr />
+      <input type="submit" value="Add" name="submit" />
+  </form>
+</body> 
+</html>
